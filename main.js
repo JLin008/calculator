f0e111bc60d4ab786e0173b20908490b8c72b64a
JLin008 <jjling0712@gmail.com>
2025-04-19 13:43:39 -0400

    added basic add, subtract, multiply, and divide functions

---
+++ main.js
@@ -0,0 +1,24 @@
+
+function add(firstNum, secondNum) {
+    return firstNum + secondNum;
+}
+
+function subtract(firstNum, secondNum) {
+    return firstNum - secondNum;
+}
+
+function multiply(firstNum, secondNum) {
+    return firstNum * secondNum;
+}
+
+function divide(firstNum, secondNum) {
+    return firstNum / secondNum;
+}
+
+console.log(add(3, 7));
+
+console.log(subtract(3, 7));
+
+console.log(multiply(3, 7));
+
+console.log(divide(6, 3));
